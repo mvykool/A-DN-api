5aae04d853b821185ef2e274b2dd101840881832
maicol hernandez <maykolandres4@gmail.com>
2023-06-25 04:46:43 -0500

    Add project files.

---
+++ Controllers/superheroController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace superhero_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class superheroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<superhero>>> GetSuperHero()
+        {
+            return new List<superhero>
+            {
+                new superhero
+                {
+                    Name = "batman",
+                    FirstName = "bruce",
+                    LastName = "wayne",
+                    Place = "gotham"
+                }
+            };
+        }
+    }
+}
---
+++ superhero.cs
@@ -0,0 +1,15 @@
+﻿namespace superhero_api
+{
+    public class superhero
+    {
+        public int Id {  get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Place { get; set; } = string.Empty;
+    }
+}
