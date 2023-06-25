60c713242384b9e5645c0308b51eb0c98249abfe
maicol hernandez <maykolandres4@gmail.com>
2023-06-25 06:10:13 -0500

    added the rest of crud

--- Controllers/superheroController.cs
+++ Controllers/superheroController.cs
@@ -21,5 +21,46 @@ namespace superhero_api.Controllers
         {
             return Ok(await _context.Superheroes.ToListAsync());
         }
+
+        [HttpPost]
+        public async Task<ActionResult<List<superhero>>> CreateSuperhero(superhero hero)
+        {
+            _context.Superheroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Superheroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<superhero>>> UpdateSuperhero(superhero hero)
+        {
+            var dbHero = await _context.Superheroes.FindAsync(hero.Id);
+
+            if (dbHero == null)
+                return BadRequest("hero not found");
+
+            dbHero.Name = hero.Name;
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.Place = hero.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Superheroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<superhero>>> DeleteHero(int id)
+        {
+            var dbHero = await _context.Superheroes.FindAsync(id);
+
+            if (dbHero == null)
+                return BadRequest("hero not found");
+
+            _context.Superheroes.Remove(dbHero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Superheroes.ToListAsync());
+        }
     }
 }
