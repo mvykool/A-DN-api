149b18bcfb682daf035792e6187c5726efb210b6
maicol hernandez <maykolandres4@gmail.com>
2023-06-25 05:33:26 -0500

    conntected sql express

--- Controllers/superheroController.cs
+++ Controllers/superheroController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using superhero_api.Data;
 
 namespace superhero_api.Controllers
 {
@@ -7,19 +9,17 @@ namespace superhero_api.Controllers
     [ApiController]
     public class superheroController : ControllerBase
     {
+        private readonly DataContext _context;
+        
+        public superheroController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<superhero>>> GetSuperHero()
         {
-            return new List<superhero>
-            {
-                new superhero
-                {
-                    Name = "batman",
-                    FirstName = "bruce",
-                    LastName = "wayne",
-                    Place = "gotham"
-                }
-            };
+            return Ok(await _context.Superheroes.ToListAsync());
         }
     }
 }
---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace superhero_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<superhero> Superheroes => Set<superhero>();
+    }
+}
